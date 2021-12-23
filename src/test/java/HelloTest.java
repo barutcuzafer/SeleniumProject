9b7b48e53309a2305e9eacf984ce439c37f6888f
barutcuzafer <barutcuzafer1@gmail.com>
2021-12-23 14:31:10 +0300

    ADDED SECOND UPDATE

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("about to share project on github");
     }
 
 }
